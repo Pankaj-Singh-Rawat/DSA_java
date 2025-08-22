4e42408efb014c16311ae1e21949034b93c1af5a
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-08-22 20:47:37 +0530

    Array two pointers problem

---
+++ arrays/TwoPointer.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoPointer {
+    public static void main(String[] args) {
+
+        // Problem 1:
+        // numbers = [2,7,11,15], target = 9
+        // Output:
+        // [1,2]
+        int[] num = { 2, 7, 11, 15 };
+        int target = 9;
+
+        System.out.println(problem1(num, target));
+
+    }
+    
+    public static int[] problem1(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int currentSum = numbers[left] + numbers[right];
+
+            if (currentSum == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[] {};
+    }
+}
