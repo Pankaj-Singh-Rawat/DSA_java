2cd5cc85c2d40a9160d64d51cd71e084c49bb65e
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-09-13 23:45:07 +0530

    problems on stack

---
+++ stacks/Ques.java
@@ -0,0 +1,38 @@
+package stacks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ques {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {4, 1, 2}, nums2 = {1, 3, 4, 2};
+        System.out.println(NextGreaterElement(nums1, nums2));
+    }
+    private static List<Integer> NextGreaterElement(int[] nums1, int[] nums2) {
+        // Question 2 (Next Greater Element I)
+
+        // Now let's try a slightly more advanced problem that uses a stack.
+
+        // Task:
+        // The next greater element of some element x in an array is the first element
+        // to the right of x that is greater than x.
+
+        // You are given two arrays of unique integers, nums1 and nums2, where nums1 is
+        // a subset of nums2. For each element in nums1, find its next greater element
+        // in nums2. If it does not exist, return -1.
+
+        // Example:
+
+        // Input: nums1 = [4, 1, 2], nums2 = [1, 3, 4, 2]
+
+        // Output: [-1, 3, -1]
+
+        List<Integer> result = new ArrayList<>();
+
+
+
+        return result;
+    }
+}
