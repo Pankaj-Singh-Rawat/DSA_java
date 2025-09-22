35be54f6fe012ad5ae71015b6ed50ff6e8f6bde3
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-09-22 22:38:51 +0530

    Probelms for trees

---
+++ Tree/Problems.java
@@ -0,0 +1,22 @@
+package Tree;
+
+public class Problems {
+    // Problem: "Invert a Binary Tree"
+
+    // To get started, try this classic problem that introduces you to tree
+    // structures and requires a recursive approach.
+
+    // Problem Statement:
+
+    // Given the root of a binary tree, invert the tree, and return its root.
+
+    // Example:
+
+    // If the input tree is:
+
+    //    4
+    //   / \
+    //   2 7
+    // / \ / \
+    // 1 3 6 9
+}
