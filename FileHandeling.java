239ebc7952fce4c83f8b6006dbfb0e95c63d2e03
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-10-12 20:19:02 +0530

    deleting the file

--- FileHandeling.java
+++ FileHandeling.java
@@ -13,27 +13,34 @@ public class FileHandeling {
     //     e.printStackTrace();
     //    }
 
-    // code to write a file
-    try{
-        FileWriter fileWriter = new FileWriter("testFile.txt");
-        fileWriter.write("This is our first line from this java course.");
-        fileWriter.close();
-    }catch(IOException e){
-        e.printStackTrace();;
-    }
+    // // code to write a file
+    // try{
+    //     FileWriter fileWriter = new FileWriter("testFile.txt");
+    //     fileWriter.write("This is our first line from this java course.");
+    //     fileWriter.close();
+    // }catch(IOException e){
+    //     e.printStackTrace();;
+    // }
 
-    // reading a file 
-    File myFile = new File("testFile.txt");
-    try{
-        Scanner sc = new Scanner(myFile);
-        while(sc.hasNextLine()){
-            String line = sc.nextLine();
-            System.out.println(line);
-        }
-        sc.close();
-    }catch(IOException e){
-        e.printStackTrace();
-    }
+    // // reading a file 
+    // File myFile = new File("testFile.txt");
+    // try{
+    //     Scanner sc = new Scanner(myFile);
+    //     while(sc.hasNextLine()){
+    //         String line = sc.nextLine();
+    //         System.out.println(line);
+    //     }
+    //     sc.close();
+    // }catch(IOException e){
+    //     e.printStackTrace();
+    // }
 
+    // how to delete a file
+    File myFile = new File("testFile.txt"); 
+    if(myFile.delete()){
+        System.out.println("I have deleted: "+ myFile.getName());
+    }else{
+        System.out.println("Some problem occured while deleting the file.");
+    }
     }
 }   
