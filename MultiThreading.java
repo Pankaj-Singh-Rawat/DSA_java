d5ed391cbe5056b82e0ce7c0cd636243618567df
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-10-13 21:20:31 +0530

    into to multithreading

---
+++ MultiThreading.java
@@ -0,0 +1,17 @@
+public class MultiThreading{
+    /*
+     * Multithreading and multiprocessing both are used to achieve multitasking.
+     * 
+     * 1. Threads use shared memory area
+     * 2. THey have faster context switching
+     * 3.Thread is light weight compared to preocess
+     * 
+     */
+    public static void main(String[] args) {
+        /*
+         * We can create and use thread in java by:
+         * a. extending thread class.
+         * b. implementing Rumlble Interface.
+         */
+    }
+}
