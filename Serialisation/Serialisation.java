bf60157cf6f6753bb0e51d0c3aca9211287d4220
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-10-14 09:09:10 +0530

    Using Serilisability

--- Serialisation/Serialisation.java
+++ Serialisation/Serialisation.java
@@ -1,4 +1,9 @@
 package Serialisation;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 public class Serialisation {
     /*
      * Serialisation: Process of converting a a object into byte stream
@@ -8,11 +13,31 @@ public class Serialisation {
      * Deserialisation: The process of converting a byte stream back into object
      * Eg: Loading a saved file
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
+                // Steps to serialise an Object:
+                /*
+                 * 1. your object should implement Serialisable Interface
+                 * 2. add java.io.Serialisable
+                 * 3. FileOutputStream fileOut = new fileOutputStream(File Path)
+                 * 4. ObjectOutputStream out = new ObjectOutputStream(file out)
+                 * 5. out.wirteObject(object name)
+                 * 6. out.close(); fileOut.close();
+                 */
+                
         User user = new User();
         user.name = "Bro";
         user.pass = "broCode";
 
-        user.sayHello();
+        FileOutputStream fileOut = new FileOutputStream("UserInfo.ser");
+
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+        out.writeObject(user);
+
+        out.close();
+        fileOut.close();
+
+        System.out.println("Object info saved.");
     }
 }
--- Serialisation/User.java
+++ Serialisation/User.java
@@ -1,6 +1,8 @@
 package Serialisation;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     String name;
     String pass;
 
