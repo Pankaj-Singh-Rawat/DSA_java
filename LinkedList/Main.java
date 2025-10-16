06a5ed2a7469db058677964e91690a4cee379bde
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-10-16 09:55:51 +0530

    implementation of doubly linked list

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -3,23 +3,31 @@ package LinkedList;
 class Main {
 
     public static void main(String[] args) {
-        LL list = new LL();
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(2);
+        // list.insertFirst(8);
+        // list.insertFirst(17);
+        // list.insertLast(99);
+
+        // list.insert(100, 3);
+        // list.display();
+
+        // list.deleteFirst();
+        // list.display();
+
+        // System.out.println(list.deleteLast());
+        // list.display();
+
+        // System.out.println(list.delete(2));
+        // list.display();
+
+        DoublyLL list = new DoublyLL();
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
-        list.insertLast(99);
 
-        list.insert(100, 3);
-        list.display();
-
-        list.deleteFirst();
-        list.display();
-
-        System.out.println(list.deleteLast());
-        list.display();
-
-        System.out.println(list.delete(2));
         list.display();
     }
 }
