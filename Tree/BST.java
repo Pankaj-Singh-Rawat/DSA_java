25d729574795dbc85ee32659da65cfea878af2d0
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-10-23 23:54:43 +0530

    binary search trees Implementation

---
+++ Tree/BST.java
@@ -0,0 +1,53 @@
+package Tree;
+
+public class BST {
+    // Binary Search Trees
+    public class Node{
+        private int value;
+        private Node left;
+        private Node right;
+         private int height;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BST(){
+
+    }
+
+    public int height(Node node){
+        if( node == null){
+            return - 1;
+        }
+
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void display(){
+        display(root, "Root Npde: ");
+    }
+
+    private void display(Node node, String details){
+        if(node == null){
+            return;
+        }
+
+        System.out.println(details + node.getValue());
+
+        display(node.left, "Left child of " + node.getValue());
+        display(node.right, "Right child of " + node.getValue());
+
+    }
+}
