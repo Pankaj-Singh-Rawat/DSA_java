e2967b59199679004fa3de508f2f2ec4c497b441
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2025-10-27 20:41:41 +0530

    intro to AVL trees

---
+++ Tree/AVL.java
@@ -0,0 +1,19 @@
+package Tree;
+
+public class AVL {
+    public static void main(String[] args) {
+        /*
+        AVL Tree: Used to balance unbalanced trees.
+            1. Insert node N normally.
+            2. Start from node N and find the node that makes the tree Unbalanced.
+             When found bottom up.
+            3. Using one of the four rules Rotate the tree.
+
+        Four Cases:
+            Left-Left: Rotate the graph to right.
+            Left-Right: Left rotate on child class then Right rotate on parent class
+            Right-Right: Left rotate parent class
+            Right-Left: Right rotate on child then left rotate on parent class
+         */
+    }
+}
