f1c2747c5bb02b0ab400c8d7ae2b263b47102e59
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-01-25 21:23:07 +0530

    Problem - 125

--- LeetCode.java
+++ LeetCode.java
@@ -302,12 +302,46 @@ public class LeetCode {
         Input: name = "alex", typed = "aaleex"
         Output: true
         Explanation: 'a' and 'e' in 'alex' were long pressed.
- */
 
         String name = "alex", typed = "aalex";
         System.out.println(isLongPressedName(name, typed));
 
+        Problem - 125
+
+        A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+        Given a string s, return true if it is a palindrome, or false otherwise.
+
+        Input: s = "A man, a plan, a canal: Panama"
+        Output: true
+        Explanation: "amanaplanacanalpanama" is a palindrome.
+ */
+
+        String input = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome1(input));
+
     }
+
+    public static boolean isPalindrome1(String s) {
+        int left = 0;
+        int right = s.length() -1;
+
+        while(left < right) {
+            if(!Character.isLetterOrDigit(s.charAt(left))) {
+                left ++;
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right --;
+            } else {
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                left ++;
+                right --;
+            }
+        }
+
+        return true;
+    }
+
 
     public static boolean isLongPressedName(String name, String typed) {
 
