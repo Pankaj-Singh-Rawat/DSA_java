f0081f94ff6a7735fdc931a275abdd918d35efd2
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-01-28 21:18:13 +0530

    Problem - 1

--- LeetCode.java
+++ LeetCode.java
@@ -1,8 +1,4 @@
-import java.lang.classfile.instruction.CharacterRange;
-import java.lang.reflect.Array;
 import java.util.*;
-
-import javax.xml.stream.events.Characters;
 
 public class LeetCode {
     public static void main(String[] args) {
@@ -330,8 +326,39 @@ public class LeetCode {
 
         String s1 = "pankaj", s2 = "";
         System.out.println(checkPermutation(s1,s2));
+
+        Problem - 1:
+
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+
+        Example 1:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  */
 
+        int[] nums = {3,3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            int required = target - nums[i];
+
+            if(map.containsKey(required)) {
+                return new int[] {map.get(required), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return null;
     }
 
     public static boolean checkPermutation(String s1, String s2) {
