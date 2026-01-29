e8b5793ebe5be635ebadc297be2b152a0eb5a571
Pankaj-Singh-Rawat <qoolphoenix@gmail.com>
2026-01-29 10:14:42 +0530

    Problem - 26

--- LeetCode.java
+++ LeetCode.java
@@ -337,12 +337,42 @@ public class LeetCode {
         Input: nums = [2,7,11,15], target = 9
         Output: [0,1]
         Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- */
-
         int[] nums = {3,3};
         int target = 6;
         System.out.println(Arrays.toString(twoSum(nums, target)));
 
+
+        Problem - 26:
+        Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+        Consider the number of unique elements in nums to be k. After removing duplicates, return the number of unique elements k.
+        The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+        Input: nums = [1,1,2]
+        Output: 2, nums = [1,2,_]
+        Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+        It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates1(arr));
+
+
+    }
+
+    public static int removeDuplicates1(int[] nums) {
+        int left = 0;
+        int right = 1;
+        while(right < nums.length) {
+            if(nums[left] != nums[right]) {
+                left++;
+                nums[left] = nums[right];
+            }
+            right ++;
+        }
+
+
+        return left + 1;
     }
 
     public static int[] twoSum(int[] nums, int target) {
